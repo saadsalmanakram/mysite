a4239ecb03338f6af3ef3f38b79d1723c488ffd7
Saad Salman <saadsalmanakram1@gmail.com>
2024-10-21 00:02:04 +0500

    Update ProjectHero.tsx

--- components/ProjectHero.tsx
+++ components/ProjectHero.tsx
@@ -5,32 +5,32 @@ const projects = [
   {
     title: "STACK Projects",
     description: "Explore innovative solutions leveraging the STACK framework, focusing on modular architecture and scalable design.",
-    link: "/",
+    link: "https://github.com/saadsalmanakram",
   },
   {
     title: "RAG Projects",
     description: "Dive into projects utilizing Retrieval-Augmented Generation (RAG) techniques to enhance content generation and retrieval efficiency.",
-    link: "/",
+    link: "https://github.com/saadsalmanakram",
   },
   {
     title: "Prompt Engineering Projects",
     description: "Engage with projects dedicated to crafting effective prompts that optimize model performance across various applications.",
-    link: "/",
+    link: "https://github.com/saadsalmanakram",
   },
   {
     title: "Fine-Tuning Projects",
     description: "Examine fine-tuning methodologies for adapting pre-trained models to specific tasks, enhancing their accuracy and utility.",
-    link: "/",
+    link: "https://github.com/saadsalmanakram",
   },
   {
     title: "Computer Vision Projects",
     description: "Discover cutting-edge computer vision projects that harness deep learning techniques for image and video analysis.",
-    link: "/",
+    link: "https://github.com/saadsalmanakram",
   },
   {
     title: "Gradio Projects",
     description: "Check out interactive applications built with Gradio, showcasing user-friendly interfaces for machine learning models.",
-    link: "/",
+    link: "https://github.com/saadsalmanakram",
   },
 ];
 
