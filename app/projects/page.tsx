fe1511a7e7bc89dc7440bd2826a8a71a9978b5dd
Saad Salman <saadsalmanakram1@gmail.com>
2024-10-20 22:58:02 +0500

    update

---
+++ app/projects/page.tsx
@@ -0,0 +1,13 @@
+import ProjectHero from "@/components/ProjectHero";
+
+export default function Project() {
+  return (
+    <main className="relative bg-black-100 flex
+     justify-center items-center flex-col overflow-hidden
+     mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <ProjectHero />
+      </div>
+    </main>
+  );
+}
