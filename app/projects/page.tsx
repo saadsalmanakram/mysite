719496d4d5b37239120101cd565d84ca73ab6c10
Saad Salman <saadsalmanakram1@gmail.com>
2024-10-21 13:12:08 +0500

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,8 +4,8 @@ export default function Project() {
   return (
     <main className="relative bg-black-100 flex
      justify-center items-center flex-col overflow-hidden
-     mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+     mx-auto sm:px-10 px-5 min-h-screen">
+      <div className="max-w-7xl w-full h-full">
         <ProjectHero />
       </div>
     </main>
