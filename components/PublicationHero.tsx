da95dce8b819dd89e02237c0ff499327fce599d7
Saad Salman <saadsalmanakram1@gmail.com>
2024-10-21 09:29:15 +0500

    Update PublicationHero.tsx

--- components/PublicationHero.tsx
+++ components/PublicationHero.tsx
@@ -7,7 +7,7 @@ const PublicationHero = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 justify-items-center">
       {/* Card 1 */}
-      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 mx-auto">
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 mx-auto min-h-[550px] flex flex-col justify-between">
         <Image
           src={`/jordans.webp`}
           alt="jordans"
@@ -15,15 +15,17 @@ const PublicationHero = () => {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-          Air Jordan 4 Retro Reimagined
-        </p>
+        <div>
+          <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+            Air Jordan 4 Retro Reimagined
+          </p>
 
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-          February 17, 2024. Your best opportunity to get these right now is by
-          entering raffles and waiting for the official releases.
-        </p>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+            February 17, 2024. Your best opportunity to get these right now is by
+            entering raffles and waiting for the official releases.
+          </p>
+        </div>
         <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
           <span>Buy now </span>
           <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
@@ -33,7 +35,7 @@ const PublicationHero = () => {
       </BackgroundGradient>
 
       {/* Card 2 */}
-      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 mx-auto">
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 mx-auto min-h-[550px] flex flex-col justify-between">
         <Image
           src={`/nike.webp`}
           alt="nike shoes"
@@ -41,14 +43,16 @@ const PublicationHero = () => {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-          Nike Air Max 90
-        </p>
+        <div>
+          <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+            Nike Air Max 90
+          </p>
 
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          The classic Nike Air Max 90 in a new colorway. Available starting
-          March 10, 2024. Don&apos;t miss out on these timeless kicks!
-        </p>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            The classic Nike Air Max 90 in a new colorway. Available starting
+            March 10, 2024. Don&apos;t miss out on these timeless kicks!
+          </p>
+        </div>
         <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
           <span>Buy now </span>
           <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
@@ -58,7 +62,7 @@ const PublicationHero = () => {
       </BackgroundGradient>
 
       {/* Card 3 */}
-      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 mx-auto">
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 mx-auto min-h-[550px] flex flex-col justify-between">
         <Image
           src={`/adidas.webp`}
           alt="adidas shoes"
@@ -66,14 +70,16 @@ const PublicationHero = () => {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-          Adidas Ultraboost 23
-        </p>
+        <div>
+          <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+            Adidas Ultraboost 23
+          </p>
 
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          The latest Adidas Ultraboost 23 promises unmatched comfort and style.
-          Available for purchase from April 5, 2024.
-        </p>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            The latest Adidas Ultraboost 23 promises unmatched comfort and style.
+            Available for purchase from April 5, 2024.
+          </p>
+        </div>
         <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
           <span>Buy now </span>
           <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
@@ -83,7 +89,7 @@ const PublicationHero = () => {
       </BackgroundGradient>
 
       {/* Card 4 */}
-      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 mx-auto">
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 mx-auto min-h-[550px] flex flex-col justify-between">
         <Image
           src={`/puma.webp`}
           alt="puma shoes"
@@ -91,14 +97,16 @@ const PublicationHero = () => {
           width="400"
           className="object-contain"
         />
-        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-          Puma RS-X Reinvent
-        </p>
+        <div>
+          <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+            Puma RS-X Reinvent
+          </p>
 
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          The Puma RS-X Reinvent offers bold style and performance. Get yours
-          starting February 28, 2024.
-        </p>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            The Puma RS-X Reinvent offers bold style and performance. Get yours
+            starting February 28, 2024.
+          </p>
+        </div>
         <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
           <span>Buy now </span>
           <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
